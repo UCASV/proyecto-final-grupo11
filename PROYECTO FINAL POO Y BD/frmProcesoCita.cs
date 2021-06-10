acd4e6719b5b7a93972e1bf4ad814a08382c7484
Br01-jac <62619073+Br01-jac@users.noreply.github.com>
2021-06-09 19:48:59 -0600

    Add files via upload

---
+++ PROYECTO FINAL POO Y BD/frmProcesoCita.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PROYECTO_FINAL_POO_Y_BD
+{
+    public partial class frmProcesoCita : Form
+    {
+        public frmProcesoCita()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void frmProcesoCita_Load(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
