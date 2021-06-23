b6dd626be0ccc8599ba1662856e1b37b8c6f2573
nestorrecinosUCA <00083120@uca.edu.sv>
2021-06-22 19:38:14 -0600

    Revert "Form validations of ADDVACINE"
    This reverts commit 855efc77a512dfee4b430d2fb00a89d3ea0ca9d6.

--- PROYECTO FINAL POO Y BD/frmAddVaccine.Designer.cs
+++ PROYECTO FINAL POO Y BD/frmAddVaccine.Designer.cs
@@ -34,7 +34,9 @@ namespace PROYECTO_FINAL_POO_Y_BD
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAddVaccine));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.txtHoraLlegada = new System.Windows.Forms.TextBox();
@@ -44,9 +46,6 @@ namespace PROYECTO_FINAL_POO_Y_BD
             this.dtFechaVacuna = new System.Windows.Forms.DateTimePicker();
             this.label4 = new System.Windows.Forms.Label();
             this.btnSave = new System.Windows.Forms.Button();
-            this.txtDUI = new System.Windows.Forms.TextBox();
-            this.btnVerificar = new System.Windows.Forms.Button();
-            this.btnCancelar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox3)).BeginInit();
@@ -55,10 +54,9 @@ namespace PROYECTO_FINAL_POO_Y_BD
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image) (resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(-3, -44);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -36);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1027, 703);
+            this.pictureBox1.Size = new System.Drawing.Size(770, 571);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -66,13 +64,19 @@ namespace PROYECTO_FINAL_POO_Y_BD
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image) (resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(-3, -2);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Location = new System.Drawing.Point(-2, -2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(517, 618);
+            this.pictureBox2.Size = new System.Drawing.Size(388, 502);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 4;
             this.pictureBox2.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 131);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(169, 20);
+            this.textBox2.TabIndex = 5;
             // 
             // label2
             // 
@@ -80,12 +84,26 @@ namespace PROYECTO_FINAL_POO_Y_BD
             this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(16, 137);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(12, 111);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(105, 21);
+            this.label2.Size = new System.Drawing.Size(79, 17);
             this.label2.TabIndex = 6;
             this.label2.Text = "Digitar DUI:";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Turquoise;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(187, 131);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 20);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Verificar";
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // label1
             // 
@@ -93,10 +111,9 @@ namespace PROYECTO_FINAL_POO_Y_BD
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(16, 278);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(12, 226);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(329, 25);
+            this.label1.Size = new System.Drawing.Size(247, 20);
             this.label1.TabIndex = 9;
             this.label1.Text = "Fecha y hora de llegada";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -107,61 +124,51 @@ namespace PROYECTO_FINAL_POO_Y_BD
             this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(16, 369);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(12, 300);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(329, 26);
+            this.label3.Size = new System.Drawing.Size(247, 21);
             this.label3.TabIndex = 10;
             this.label3.Text = "Fecha y hora de vacuna";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // txtHoraLlegada
             // 
-            this.txtHoraLlegada.Enabled = false;
-            this.txtHoraLlegada.Location = new System.Drawing.Point(185, 320);
-            this.txtHoraLlegada.Margin = new System.Windows.Forms.Padding(4);
+            this.txtHoraLlegada.Location = new System.Drawing.Point(139, 260);
             this.txtHoraLlegada.Name = "txtHoraLlegada";
-            this.txtHoraLlegada.Size = new System.Drawing.Size(159, 22);
+            this.txtHoraLlegada.Size = new System.Drawing.Size(120, 20);
             this.txtHoraLlegada.TabIndex = 12;
             // 
             // pictureBox3
             // 
             this.pictureBox3.Image = ((System.Drawing.Image) (resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(785, 6);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox3.Location = new System.Drawing.Point(589, 5);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(205, 180);
+            this.pictureBox3.Size = new System.Drawing.Size(154, 146);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 20;
             this.pictureBox3.TabStop = false;
             // 
             // txtHoraVacuna
             // 
-            this.txtHoraVacuna.Enabled = false;
-            this.txtHoraVacuna.Location = new System.Drawing.Point(185, 399);
-            this.txtHoraVacuna.Margin = new System.Windows.Forms.Padding(4);
+            this.txtHoraVacuna.Location = new System.Drawing.Point(139, 324);
             this.txtHoraVacuna.Name = "txtHoraVacuna";
-            this.txtHoraVacuna.Size = new System.Drawing.Size(159, 22);
+            this.txtHoraVacuna.Size = new System.Drawing.Size(120, 20);
             this.txtHoraVacuna.TabIndex = 21;
             // 
             // dtFechaLlegada
             // 
-            this.dtFechaLlegada.Enabled = false;
             this.dtFechaLlegada.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtFechaLlegada.Location = new System.Drawing.Point(19, 320);
-            this.dtFechaLlegada.Margin = new System.Windows.Forms.Padding(4);
+            this.dtFechaLlegada.Location = new System.Drawing.Point(14, 260);
             this.dtFechaLlegada.Name = "dtFechaLlegada";
-            this.dtFechaLlegada.Size = new System.Drawing.Size(157, 22);
+            this.dtFechaLlegada.Size = new System.Drawing.Size(119, 20);
             this.dtFechaLlegada.TabIndex = 22;
             // 
             // dtFechaVacuna
             // 
-            this.dtFechaVacuna.Enabled = false;
             this.dtFechaVacuna.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtFechaVacuna.Location = new System.Drawing.Point(19, 399);
-            this.dtFechaVacuna.Margin = new System.Windows.Forms.Padding(4);
+            this.dtFechaVacuna.Location = new System.Drawing.Point(14, 324);
             this.dtFechaVacuna.Name = "dtFechaVacuna";
-            this.dtFechaVacuna.Size = new System.Drawing.Size(157, 22);
+            this.dtFechaVacuna.Size = new System.Drawing.Size(119, 20);
             this.dtFechaVacuna.TabIndex = 23;
             // 
             // label4
@@ -170,10 +177,9 @@ namespace PROYECTO_FINAL_POO_Y_BD
             this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(-3, -2);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(-2, -2);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(411, 98);
+            this.label4.Size = new System.Drawing.Size(308, 80);
             this.label4.TabIndex = 24;
             this.label4.Text = "Proceso de vacunación";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -181,71 +187,23 @@ namespace PROYECTO_FINAL_POO_Y_BD
             // btnSave
             // 
             this.btnSave.BackColor = System.Drawing.Color.Turquoise;
-            this.btnSave.Enabled = false;
             this.btnSave.FlatAppearance.BorderColor = System.Drawing.Color.White;
             this.btnSave.FlatAppearance.BorderSize = 0;
             this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.btnSave.ForeColor = System.Drawing.Color.White;
-            this.btnSave.Location = new System.Drawing.Point(236, 465);
-            this.btnSave.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSave.Location = new System.Drawing.Point(187, 359);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(125, 41);
+            this.btnSave.Size = new System.Drawing.Size(72, 20);
             this.btnSave.TabIndex = 25;
             this.btnSave.Text = "Guardar";
             this.btnSave.UseVisualStyleBackColor = false;
-            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
-            // 
-            // txtDUI
-            // 
-            this.txtDUI.Location = new System.Drawing.Point(13, 164);
-            this.txtDUI.Margin = new System.Windows.Forms.Padding(4);
-            this.txtDUI.Name = "txtDUI";
-            this.txtDUI.Size = new System.Drawing.Size(229, 22);
-            this.txtDUI.TabIndex = 26;
-            // 
-            // btnVerificar
-            // 
-            this.btnVerificar.BackColor = System.Drawing.Color.Turquoise;
-            this.btnVerificar.FlatAppearance.BorderColor = System.Drawing.Color.White;
-            this.btnVerificar.FlatAppearance.BorderSize = 0;
-            this.btnVerificar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnVerificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.btnVerificar.ForeColor = System.Drawing.Color.White;
-            this.btnVerificar.Location = new System.Drawing.Point(265, 164);
-            this.btnVerificar.Margin = new System.Windows.Forms.Padding(4);
-            this.btnVerificar.Name = "btnVerificar";
-            this.btnVerificar.Size = new System.Drawing.Size(96, 25);
-            this.btnVerificar.TabIndex = 27;
-            this.btnVerificar.Text = "Verificar";
-            this.btnVerificar.UseVisualStyleBackColor = false;
-            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
-            // 
-            // btnCancelar
-            // 
-            this.btnCancelar.BackColor = System.Drawing.Color.LightSeaGreen;
-            this.btnCancelar.FlatAppearance.BorderColor = System.Drawing.Color.White;
-            this.btnCancelar.FlatAppearance.BorderSize = 0;
-            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.btnCancelar.ForeColor = System.Drawing.Color.White;
-            this.btnCancelar.Location = new System.Drawing.Point(60, 465);
-            this.btnCancelar.Margin = new System.Windows.Forms.Padding(4);
-            this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(125, 41);
-            this.btnCancelar.TabIndex = 28;
-            this.btnCancelar.Text = "Cancelar";
-            this.btnCancelar.UseVisualStyleBackColor = false;
-            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
             // frmAddVaccine
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1007, 615);
-            this.Controls.Add(this.btnCancelar);
-            this.Controls.Add(this.btnVerificar);
-            this.Controls.Add(this.txtDUI);
+            this.ClientSize = new System.Drawing.Size(755, 500);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.dtFechaVacuna);
@@ -255,10 +213,11 @@ namespace PROYECTO_FINAL_POO_Y_BD
             this.Controls.Add(this.txtHoraLlegada);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmAddVaccine";
             this.Text = "frmAddVaccine";
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).EndInit();
@@ -267,14 +226,6 @@ namespace PROYECTO_FINAL_POO_Y_BD
             this.ResumeLayout(false);
             this.PerformLayout();
         }
-
-        private System.Windows.Forms.Button btnCancelar;
-
-        private System.Windows.Forms.Button button1;
-
-        private System.Windows.Forms.TextBox txtDUI;
-        private System.Windows.Forms.Button btnVerificar;
-
 
         private System.Windows.Forms.Button btnSave;
 
@@ -296,6 +247,7 @@ namespace PROYECTO_FINAL_POO_Y_BD
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
 
+        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button2;
 
